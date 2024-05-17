0de9faf0366255c039185b50e92689998f3ffced
denis rodrigues dos santos <denisrodrigues.ita@gmail.com>
2024-05-17 13:53:29 -0300

    chore: Update header and hero styles for responsive design

--- js/modules/slider.js
+++ js/modules/slider.js
@@ -1,6 +1,20 @@
 export const slider = (slide) => {
+  let intervalId;
   const slides = document.querySelector(slide);
   const slideWidth = slides.children[0].offsetWidth;
+  let currentSlideIndex = 0;
+
+  function showSlide() {
+    const totalSlides = slides.children.length;
+
+    for (let i = 0; i < totalSlides; i++) {
+      slides.children[i].style.display = "none";
+    }
+
+    slides.children[currentSlideIndex].style.display = "block";
+    currentSlideIndex = (currentSlideIndex + 1) % totalSlides;
+  }
+
   function moveSlides() {
     slides.appendChild(slides.firstElementChild.cloneNode(true));
 
@@ -15,5 +29,36 @@ export const slider = (slide) => {
       slides.removeChild(slides.firstElementChild);
     }, 500);
   }
-  setInterval(moveSlides, 3000);
+
+  function setupSlider() {
+    const isMobile = window.matchMedia("(max-width: 480px)").matches;
+
+    clearInterval(intervalId);
+
+    if (isMobile) {
+      const totalSlides = slides.children.length;
+
+      for (let i = 0; i < totalSlides; i++) {
+        slides.children[i].style.display = "none";
+      }
+
+      currentSlideIndex = 0;
+      showSlide();
+      intervalId = setInterval(showSlide, 3000);
+    } else {
+      const totalSlides = slides.children.length;
+
+      for (let i = 0; i < totalSlides; i++) {
+        slides.children[i].style.display = "block";
+      }
+
+      intervalId = setInterval(moveSlides, 3000);
+    }
+  }
+
+  setupSlider();
+
+  window.addEventListener('resize', () => {
+    setupSlider();
+  });
 };
